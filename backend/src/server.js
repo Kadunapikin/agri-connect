cc5c3b55f1a2741bcc080a85984a5a11170f9336
Kadunapikin <ibakoshi27@gmail.com>
2024-03-04 20:13:26 +0100

    modify server.js to use the productRoutes

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const productRoutes = require('./routes/products');// Import the product routes
+
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+
+// Use the productRoutes for any requests to /api/products
+app.use('/api/products', productRoutes);
+
 
 app.get('/', (req, res) => {
   res.send('AgriConnect API is running...');
